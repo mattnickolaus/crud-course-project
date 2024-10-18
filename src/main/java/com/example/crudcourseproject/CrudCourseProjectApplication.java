8d6d20bb55f5a737581371a88a2432361089abce
mattnickolaus <mattnickolaus@gmail.com>
2024-10-17 23:42:28 -0400

    First commit

---
+++ src/main/java/com/example/crudcourseproject/CrudCourseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudcourseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCourseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudCourseProjectApplication.class, args);
+    }
+
+}
